fee9a51dd55c12b63b3912f27e1527f00b6f0fa2
Elaine Herrera <elainehdelcueto@gmail.com>
2025-12-18 13:08:16 +0100

    Create DefaultPlantCareModel.swift

---
+++ FoundationModelsKit/Domain/Models/DefaultPlantCareModel.swift
@@ -0,0 +1,44 @@
+//
+//  DefaultPlantCareModel.swift
+//  FoundationModelsTest
+//
+//  Created by Elaine Herrera on 18/12/25.
+//
+import FoundationModels
+
+final class DefaultPlantCareModel: PlantCareModel {
+    let model: SystemLanguageModel
+    
+    let instructions = """
+    Given the following plant care context, determine an appropriate watering schedule.
+    Consider horticulture best practices.
+    """
+    
+    init(model: SystemLanguageModel = .default) {
+        self.model = model
+    }
+    
+    func generateWateringAdvice(for context: PlantCareContext) async throws -> WateringAdvice {
+        
+        guard model.isAvailable else {
+            throw ModelError.modelUnavailable
+        }
+        
+        do {
+            let session = LanguageModelSession(
+                model: model,
+                instructions: instructions
+            )
+            
+            let response = try await session.respond(
+                to: .init(context),
+                generating: WateringAdvice.self
+            )
+            
+            return response.content
+            
+        } catch {
+            throw ModelError.generationFailed(underlying: error)
+        }
+    }
+}
