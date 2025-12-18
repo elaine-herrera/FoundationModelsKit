0c715065d97a549ad8ca62569a73b81f5a725afe
Elaine Herrera <elainehdelcueto@gmail.com>
2025-12-18 12:48:14 +0100

    Initial Commit

---
+++ FoundationModelsKit/FoundationModelsKit.swift
@@ -0,0 +1,9 @@
+//
+//  FoundationModelsKit.swift
+//  FoundationModelsKit
+//
+//  Created by Elaine Herrera on 18/12/25.
+//
+
+import Foundation
+
---
+++ FoundationModelsKitTests/FoundationModelsKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoundationModelsKitTests.swift
+//  FoundationModelsKitTests
+//
+//  Created by Elaine Herrera on 18/12/25.
+//
+
+import Testing
+@testable import FoundationModelsKit
+
+struct FoundationModelsKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
