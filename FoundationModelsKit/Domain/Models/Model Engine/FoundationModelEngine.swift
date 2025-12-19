48e1f312c7613c9bd7911cac430a29bf46b65901
Elaine Herrera <elainehdelcueto@gmail.com>
2025-12-19 12:45:22 +0100

    Update FoundationModelEngine.swift

--- FoundationModelsKit/Domain/Models/Model Engine/FoundationModelEngine.swift
+++ FoundationModelsKit/Domain/Models/Model Engine/FoundationModelEngine.swift
@@ -7,21 +7,21 @@
 import FoundationModels
 
 public final class FoundationModelEngine: ModelEngine {
-    private let model: SystemLanguageModel
+    public let model: SystemLanguageModel
     
     public init(model: SystemLanguageModel = .default) {
         self.model = model
     }
     
-    var isAvailable: Bool {
+    public var isAvailable: Bool {
         model.isAvailable
     }
     
-    var availability: SystemLanguageModel.Availability {
+    public var availability: SystemLanguageModel.Availability {
         model.availability
     }
     
-    func generate<Response: Generable>(instructions: String, input: some Generable,
+    public func generate<Response: Generable>(instructions: String, input: some Generable,
                                        responseType: Response.Type) async throws -> Response {
         let session = LanguageModelSession(model: model, instructions: instructions)
         let response = try await session.respond(to: .init(input), generating: responseType)
